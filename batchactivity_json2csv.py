290dd111083df4d2b045a82b0ba105b804128610
mustafaddogann <mstf2202@gmail.com>
2025-06-11 01:58:49 -0700

    feat: Add cffi backend and dependencies for ijson

--- batchactivity_json2csv.py
+++ batchactivity_json2csv.py
@@ -15,10 +15,15 @@ import itertools
 
 # Recommended: Use the faster C backend if available
 try:
-    import ijson.backends.yajl2_c as ijson_backend
+    import ijson.backends.yajl2_cffi as ijson_backend
+    print("Using yajl2_cffi ijson backend.")
 except ImportError:
-    import ijson.backends.python as ijson_backend
-    print("Warning: C backend for ijson not found. Falling back to slower Python backend.")
+    try:
+        import ijson.backends.yajl2_c as ijson_backend
+        print("Using yajl2_c ijson backend.")
+    except ImportError:
+        import ijson.backends.python as ijson_backend
+        print("Warning: C backend for ijson not found. Falling back to slower Python backend.")
 
 def parse_args():
     """Parses command-line arguments."""
